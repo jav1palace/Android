a522c4a25de0d9d54a1aeed35edca66e21febc7a
Javier Palacio <jav1palace@gmail.com>
2016-12-06 23:53:23 +0100

    GI-014 - Cinema resource implemented.

--- app/src/main/java/com/example/javierpalaciocuenca/myapplication/ui/activities/ServiceListActivity.java
+++ app/src/main/java/com/example/javierpalaciocuenca/myapplication/ui/activities/ServiceListActivity.java
@@ -15,6 +15,7 @@ import com.example.javierpalaciocuenca.myapplication.resources.JSONResource;
 import com.example.javierpalaciocuenca.myapplication.resources.impl.ATMSource;
 import com.example.javierpalaciocuenca.myapplication.resources.impl.BowlingSource;
 import com.example.javierpalaciocuenca.myapplication.resources.impl.CasinoSource;
+import com.example.javierpalaciocuenca.myapplication.resources.impl.CinemaSource;
 import com.example.javierpalaciocuenca.myapplication.resources.impl.CitizenATMSource;
 import com.example.javierpalaciocuenca.myapplication.resources.impl.RecreationalAreaSource;
 import com.example.javierpalaciocuenca.myapplication.ui.activities.utils.MapItem;
@@ -44,7 +45,7 @@ public class ServiceListActivity extends Activity {
             put("ATMs", ATMSource.class);
             put("Recreational Areas", RecreationalAreaSource.class);
             put("Casinos", CasinoSource.class);
-            put("Wordpress", null);
+            put("Cinemas", CinemaSource.class);
             put("Drupal", null);
             put("Twitter", null);
         }
